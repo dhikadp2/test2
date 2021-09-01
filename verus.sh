7ef2d0940185efab0a07285c1d3cb3e7131fda4f
dhikadp2 <89255497+dhikadp2@users.noreply.github.com>
2021-09-02 06:56:42 +0700

    Update verus.sh

--- verus.sh
+++ verus.sh
@@ -1,4 +1,4 @@
-sudo apt update
+#verus
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 tar xf hellminer_cpu_linux.tar.gz
 ./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u R9zbcqxx11oyVH5c9JTcMthuHAi6QUyJws.rig001 -p x --cpu 4
